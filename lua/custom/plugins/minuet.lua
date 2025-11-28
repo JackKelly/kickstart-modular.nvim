d6b75ab274b460b1cb7c6faa91249baf85c1dfb6
Jack Kelly <jack@openclimatefix.org>
2025-11-28 18:22:42 +0000

    Manually trigger minuet

--- lua/custom/plugins/minuet.lua
+++ lua/custom/plugins/minuet.lua
@@ -3,7 +3,7 @@ return {
     'milanglacier/minuet-ai.nvim',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      'saghen/blink.cmp',
+      -- 'saghen/blink.cmp',
     },
     config = function()
       require('minuet').setup {
--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -30,85 +30,91 @@ return {
         opts = {},
       },
       'folke/lazydev.nvim',
+      { 'milanglacier/minuet-ai.nvim' },
     },
     --- @module 'blink.cmp'
     --- @type blink.cmp.Config
-    opts = {
-      keymap = {
-        -- 'default' (recommended) for mappings similar to built-in completions
-        --   <c-y> to accept ([y]es) the completion.
-        --    This will auto-import if your LSP supports it.
-        --    This will expand snippets if the LSP sent a snippet.
-        -- 'super-tab' for tab to accept
-        -- 'enter' for enter to accept
-        -- 'none' for no mappings
-        --
-        -- For an understanding of why the 'default' preset is recommended,
-        -- you will need to read `:help ins-completion`
-        --
-        -- No, but seriously. Please read `:help ins-completion`, it is really good!
-        --
-        -- All presets have the following mappings:
-        -- <tab>/<s-tab>: move to right/left of your snippet expansion
-        -- <c-space>: Open menu or open docs if already open
-        -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-        -- <c-e>: Hide menu
-        -- <c-k>: Toggle signature help
-        --
-        -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'default',
+    opts = function()
+      return {
+        keymap = {
+          -- 'default' (recommended) for mappings similar to built-in completions
+          --   <c-y> to accept ([y]es) the completion.
+          --    This will auto-import if your LSP supports it.
+          --    This will expand snippets if the LSP sent a snippet.
+          -- 'super-tab' for tab to accept
+          -- 'enter' for enter to accept
+          -- 'none' for no mappings
+          --
+          -- For an understanding of why the 'default' preset is recommended,
+          -- you will need to read `:help ins-completion`
+          --
+          -- No, but seriously. Please read `:help ins-completion`, it is really good!
+          --
+          -- All presets have the following mappings:
+          -- <tab>/<s-tab>: move to right/left of your snippet expansion
+          -- <c-space>: Open menu or open docs if already open
+          -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
+          -- <c-e>: Hide menu
+          -- <c-k>: Toggle signature help
+          --
+          -- See :h blink-cmp-config-keymap for defining your own keymap
+          preset = 'default',
 
-        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-      },
+          -- Manually invoke minuet completion.
+          ['<A-y>'] = require('minuet').make_blink_map(),
 
-      appearance = {
-        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = 'mono',
-      },
+          -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+          --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+        },
 
-      completion = {
-        -- By default, you may press `<c-space>` to show the documentation.
-        -- Optionally, set `auto_show = true` to show the documentation after a delay.
-        documentation = { auto_show = false, auto_show_delay_ms = 500 },
+        appearance = {
+          -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+          -- Adjusts spacing to ensure icons are aligned
+          nerd_font_variant = 'mono',
+        },
 
-        -- Recommended by minuet documentation to avoid unnecessary request:
-        trigger = { prefetch_on_insert = false },
-      },
+        completion = {
+          -- By default, you may press `<c-space>` to show the documentation.
+          -- Optionally, set `auto_show = true` to show the documentation after a delay.
+          documentation = { auto_show = false, auto_show_delay_ms = 500 },
 
-      sources = {
-        default = { 'lsp', 'path', 'snippets', 'lazydev' }, -- 'minuet', 'codecompanion'
-        providers = {
-          lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+          -- Recommended by minuet documentation to avoid unnecessary request:
+          trigger = { prefetch_on_insert = false },
+        },
 
-          -- For manual completion only, remove 'minuet' from default
-          minuet = {
-            name = 'minuet',
-            module = 'minuet.blink',
-            async = true,
-            -- Should match minuet.config.request_timeout * 1000,
-            -- since minuet.config.request_timeout is in seconds
-            timeout_ms = 3000,
-            score_offset = 50, -- Gives minuet higher priority among suggestions
+        sources = {
+          default = { 'lsp', 'path', 'snippets', 'lazydev' }, -- 'minuet', 'codecompanion'
+          providers = {
+            lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+
+            -- For manual completion only, remove 'minuet' from default
+            minuet = {
+              name = 'minuet',
+              module = 'minuet.blink',
+              async = true,
+              -- Should match minuet.config.request_timeout * 1000,
+              -- since minuet.config.request_timeout is in seconds
+              timeout_ms = 3000,
+              score_offset = 50, -- Gives minuet higher priority among suggestions
+            },
           },
         },
-      },
 
-      snippets = { preset = 'luasnip' },
+        snippets = { preset = 'luasnip' },
 
-      -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
-      -- which automatically downloads a prebuilt binary when enabled.
-      --
-      -- By default, we use the Lua implementation instead, but you may enable
-      -- the rust implementation via `'prefer_rust_with_warning'`
-      --
-      -- See :h blink-cmp-config-fuzzy for more information
-      fuzzy = { implementation = 'prefer_rust_with_warning' },
+        -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
+        -- which automatically downloads a prebuilt binary when enabled.
+        --
+        -- By default, we use the Lua implementation instead, but you may enable
+        -- the rust implementation via `'prefer_rust_with_warning'`
+        --
+        -- See :h blink-cmp-config-fuzzy for more information
+        fuzzy = { implementation = 'prefer_rust_with_warning' },
 
-      -- Shows a signature help window while you type arguments for a function
-      signature = { enabled = true },
-    },
+        -- Shows a signature help window while you type arguments for a function
+        signature = { enabled = true },
+      }
+    end,
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
