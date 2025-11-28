49f234ac81bad8dce5f4c44e7873eaf3b384d527
Jack Kelly <jack@openclimatefix.org>
2025-11-28 18:29:51 +0000

    Use C-g to trigger Gemini completion

--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -61,7 +61,7 @@ return {
           preset = 'default',
 
           -- Manually invoke minuet completion.
-          ['<A-y>'] = require('minuet').make_blink_map(),
+          ['<C-g>'] = require('minuet').make_blink_map(),
 
           -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
           --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
